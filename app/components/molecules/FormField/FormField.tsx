a5c528352e2ec548a43e8ff59e112f88baa5b8c9
chelladurai-1997 <mechelladurai97@gmail.com>
2024-09-02 01:25:24 +0530

    feat: register form ui seperator added

--- app/components/molecules/FormField/FormField.tsx
+++ app/components/molecules/FormField/FormField.tsx
@@ -74,7 +74,7 @@ const FormField: React.FC<FormFieldProps> = ({
           className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
           id="profile_bio"
           name="profile_bio"
-          placeholder="Profile biography"
+          placeholder="Profile Biography"
           required
         />
       </>
--- app/components/organism/Banner/Banner.tsx
+++ app/components/organism/Banner/Banner.tsx
@@ -4,9 +4,10 @@ const Banner: React.FC = () => (
       <ul className="overflow-hidden">
         <li className="w-full">
           <img
-            src="https://qph.cf2.quoracdn.net/main-qimg-a426fc6fb067ce5cfcc4fc060c2b756c-c"
+            // src="https://qph.cf2.quoracdn.net/main-qimg-a426fc6fb067ce5cfcc4fc060c2b756c-c"
+            src="https://images.unsplash.com/photo-1678705902081-5d55ab847046?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHRhbWlsJTIwd2VkZGluZ3xlbnwwfHwwfHx8MA%3D%3D"
             alt="Banner"
-            className="w-full object-cover h-64"
+            className="w-full object-contain h-64"
           />
         </li>
       </ul>
