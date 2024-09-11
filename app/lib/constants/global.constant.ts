bff91ffbfc34d1c92ac480b5ee8000e8fd18dab9
chelladurai-1997 <mechelladurai97@gmail.com>
2024-09-11 22:17:46 +0530

    feat: static options update

--- app/lib/constants/global.constant.ts
+++ app/lib/constants/global.constant.ts
@@ -100,6 +100,7 @@ export const maritalStatusOptions = [
 ];
 
 export const noOfChildrensOptions = [
+  { value: "0", label: "0" },
   { value: "1", label: "1" },
   { value: "2", label: "2" },
   { value: "3", label: "3" },
