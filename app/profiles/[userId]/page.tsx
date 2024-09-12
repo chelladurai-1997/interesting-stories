d1edb533020130843c2911cb129008b7bcfc282c
chelladurai-1997 <mechelladurai97@gmail.com>
2024-09-13 01:56:48 +0530

    feat: dynamic route update

--- app/profiles/[userId]/page.tsx
+++ app/profiles/[userId]/page.tsx
@@ -198,7 +198,7 @@ const Page: React.FC = () => {
     <div className="p-6 space-y-4">
       <div className="flex justify-center">
         <Image
-          src={profile.contactInfo.photo}
+          src={profile?.contactInfo?.photo}
           alt="Profile"
           className="w-32 h-32 rounded-full object-cover"
           width={128}
@@ -218,27 +218,27 @@ const Page: React.FC = () => {
             <strong>Name:</strong> {profile.basicInfo.name}
           </p>
           <p>
-            <strong>Date of Birth:</strong> {profile.basicInfo.dob}
+            <strong>Date of Birth:</strong> {profile?.basicInfo?.dob}
           </p>
           <p>
-            <strong>Gender:</strong> {profile.basicInfo.gender}
+            <strong>Gender:</strong> {profile?.basicInfo?.gender}
           </p>
           <p>
             <strong>Profile Created By:</strong>{" "}
-            {profile.basicInfo.profile_created_by}
+            {profile.basicInfo?.profile_created_by}
           </p>
           <p>
-            <strong>Marital Status:</strong> {profile.basicInfo.marital_status}
+            <strong>Marital Status:</strong> {profile.basicInfo?.marital_status}
           </p>
           <p>
-            <strong>Children:</strong> {profile.basicInfo.children}
+            <strong>Children:</strong> {profile.basicInfo?.children}
           </p>
           <p>
             <strong>Children Living Status:</strong>{" "}
-            {profile.basicInfo.children_living_status}
+            {profile.basicInfo?.children_living_status}
           </p>
           <p>
-            <strong>Profile Bio:</strong> {profile.basicInfo.profile_bio}
+            <strong>Profile Bio:</strong> {profile.basicInfo?.profile_bio}
           </p>
         </div>
       </Accordion>
@@ -252,25 +252,25 @@ const Page: React.FC = () => {
       >
         <div className="space-y-2 text-gray-800">
           <p>
-            <strong>Mobile:</strong> {profile.contactInfo.mobile}
+            <strong>Mobile:</strong> {profile.contactInfo?.mobile}
           </p>
           <p>
-            <strong>WhatsApp:</strong> {profile.contactInfo.whatsapp}
+            <strong>WhatsApp:</strong> {profile.contactInfo?.whatsapp}
           </p>
           <p>
-            <strong>Country:</strong> {profile.contactInfo.country}
+            <strong>Country:</strong> {profile.contactInfo?.country}
           </p>
           <p>
-            <strong>State:</strong> {profile.contactInfo.state}
+            <strong>State:</strong> {profile.contactInfo?.state}
           </p>
           <p>
-            <strong>District:</strong> {profile.contactInfo.district}
+            <strong>District:</strong> {profile.contactInfo?.district}
           </p>
           <p>
-            <strong>Address:</strong> {profile.contactInfo.address}
+            <strong>Address:</strong> {profile.contactInfo?.address}
           </p>
           <p>
-            <strong>Pin Code:</strong> {profile.contactInfo.pin_code}
+            <strong>Pin Code:</strong> {profile.contactInfo?.pin_code}
           </p>
         </div>
       </Accordion>
@@ -286,27 +286,27 @@ const Page: React.FC = () => {
       >
         <div className="space-y-2 text-gray-800">
           <p>
-            <strong>Education:</strong> {profile.educationOccupation.education}
+            <strong>Education:</strong> {profile.educationOccupation?.education}
           </p>
           <p>
             <strong>Education Info:</strong>{" "}
-            {profile.educationOccupation.educationInfo}
+            {profile.educationOccupation?.educationInfo}
           </p>
           <p>
             <strong>Occupation:</strong>{" "}
-            {profile.educationOccupation.occupation}
+            {profile.educationOccupation?.occupation}
           </p>
           <p>
             <strong>Occupation Info:</strong>{" "}
-            {profile.educationOccupation.occupationInfo}
+            {profile.educationOccupation?.occupationInfo}
           </p>
           <p>
             <strong>Working Place:</strong>{" "}
-            {profile.educationOccupation.workingPlace}
+            {profile.educationOccupation?.workingPlace}
           </p>
           <p>
             <strong>Monthly Income:</strong>{" "}
-            {profile.educationOccupation.monthlyIncome}
+            {profile.educationOccupation?.monthlyIncome}
           </p>
         </div>
       </Accordion>
@@ -320,22 +320,23 @@ const Page: React.FC = () => {
       >
         <div className="space-y-2 text-gray-800">
           <p>
-            <strong>Jaadhagam:</strong> {profile.expectations.jaadhagam}
+            <strong>Jaadhagam:</strong> {profile.expectations?.jaadhagam}
           </p>
           <p>
             <strong>Marital Status:</strong>{" "}
-            {profile.expectations.marital_status}
+            {profile.expectations?.marital_status}
           </p>
           <p>
-            <strong>Working Place:</strong> {profile.expectations.working_place}
+            <strong>Working Place:</strong>{" "}
+            {profile.expectations?.working_place}
           </p>
           <p>
             <strong>Expecting Stars:</strong>{" "}
-            {profile.expectations.expecting_stars}
+            {profile.expectations?.expecting_stars}
           </p>
           <p>
             <strong>Expectation Info:</strong>{" "}
-            {profile.expectations.expectation_info}
+            {profile.expectations?.expectation_info}
           </p>
         </div>
       </Accordion>
@@ -349,52 +350,55 @@ const Page: React.FC = () => {
       >
         <div className="space-y-2 text-gray-800">
           <p>
-            <strong>Father's Name:</strong> {profile.familyDetails.fatherName}
+            <strong>Father's Name:</strong> {profile.familyDetails?.fatherName}
           </p>
           <p>
-            <strong>Father Status:</strong> {profile.familyDetails.fatherStatus}
+            <strong>Father Status:</strong>{" "}
+            {profile.familyDetails?.fatherStatus}
           </p>
           <p>
-            <strong>Mother's Name:</strong> {profile.familyDetails.motherName}
+            <strong>Mother's Name:</strong> {profile.familyDetails?.motherName}
           </p>
           <p>
-            <strong>Mother Status:</strong> {profile.familyDetails.motherStatus}
+            <strong>Mother Status:</strong>{" "}
+            {profile.familyDetails?.motherStatus}
           </p>
           <p>
             <strong>Father's Occupation:</strong>{" "}
-            {profile.familyDetails.fatherOccupation}
+            {profile.familyDetails?.fatherOccupation}
           </p>
           <p>
             <strong>Mother's Occupation:</strong>{" "}
-            {profile.familyDetails.motherOccupation}
+            {profile.familyDetails?.motherOccupation}
           </p>
           <p>
-            <strong>Mother's Kulam:</strong> {profile.familyDetails.motherKulam}
+            <strong>Mother's Kulam:</strong>{" "}
+            {profile.familyDetails?.motherKulam}
           </p>
           <p>
-            <strong>Living Place:</strong> {profile.familyDetails.livingPlace}
+            <strong>Living Place:</strong> {profile.familyDetails?.livingPlace}
           </p>
           <p>
-            <strong>Native Place:</strong> {profile.familyDetails.nativePlace}
+            <strong>Native Place:</strong> {profile.familyDetails?.nativePlace}
           </p>
           <p>
             <strong>Number of Brothers:</strong>{" "}
-            {profile.familyDetails.noOfBrothers}
+            {profile.familyDetails?.noOfBrothers}
           </p>
           <p>
             <strong>Number of Brothers Married:</strong>{" "}
-            {profile.familyDetails.noOfBrothersMarried}
+            {profile.familyDetails?.noOfBrothersMarried}
           </p>
           <p>
             <strong>Number of Sisters:</strong>{" "}
-            {profile.familyDetails.noOfSisters}
+            {profile.familyDetails?.noOfSisters}
           </p>
           <p>
             <strong>Number of Sisters Married:</strong>{" "}
-            {profile.familyDetails.noOfSistersMarried}
+            {profile.familyDetails?.noOfSistersMarried}
           </p>
           <p>
-            <strong>Property:</strong> {profile.familyDetails.property}
+            <strong>Property:</strong> {profile.familyDetails?.property}
           </p>
         </div>
       </Accordion>
@@ -408,24 +412,24 @@ const Page: React.FC = () => {
       >
         <div className="space-y-2 text-gray-800">
           <p>
-            <strong>Raasi:</strong> {profile.horoscopeInfo.raasi}
+            <strong>Raasi:</strong> {profile.horoscopeInfo?.raasi}
           </p>
           <p>
-            <strong>Nachathiram:</strong> {profile.horoscopeInfo.nachathiram}
+            <strong>Nachathiram:</strong> {profile.horoscopeInfo?.nachathiram}
           </p>
           <p>
-            <strong>Lagnam:</strong> {profile.horoscopeInfo.lagnam}
+            <strong>Lagnam:</strong> {profile.horoscopeInfo?.lagnam}
           </p>
           <p>
-            <strong>Dhisai Irupu:</strong> {profile.horoscopeInfo.dhisaiIrupu}
+            <strong>Dhisai Irupu:</strong> {profile.horoscopeInfo?.dhisaiIrupu}
           </p>
           <p>
-            <strong>Dhosam:</strong> {profile.horoscopeInfo.dhosam}
+            <strong>Dhosam:</strong> {profile.horoscopeInfo?.dhosam}
           </p>
           <div>
             <strong>Horoscope Image:</strong>{" "}
             <Image
-              src={profile.horoscopeInfo.upload}
+              src={profile.horoscopeInfo?.upload}
               alt="Horoscope"
               className="w-full h-auto rounded-md object-cover"
               width={600}
@@ -446,36 +450,36 @@ const Page: React.FC = () => {
       >
         <div className="space-y-2 text-gray-800">
           <p>
-            <strong>Religion:</strong> {profile.personalDetails.religion}
+            <strong>Religion:</strong> {profile.personalDetails?.religion}
           </p>
           <p>
-            <strong>Caste:</strong> {profile.personalDetails.caste}
+            <strong>Caste:</strong> {profile.personalDetails?.caste}
           </p>
           <p>
-            <strong>Kulam:</strong> {profile.personalDetails.kulam}
+            <strong>Kulam:</strong> {profile.personalDetails?.kulam}
           </p>
           <p>
-            <strong>Kula Deivam:</strong> {profile.personalDetails.kula_deivam}
+            <strong>Kula Deivam:</strong> {profile.personalDetails?.kula_deivam}
           </p>
           <p>
-            <strong>Height:</strong> {profile.personalDetails.height}
+            <strong>Height:</strong> {profile.personalDetails?.height}
           </p>
           <p>
-            <strong>Complexion:</strong> {profile.personalDetails.complexion}
+            <strong>Complexion:</strong> {profile.personalDetails?.complexion}
           </p>
           <p>
-            <strong>Weight:</strong> {profile.personalDetails.weight}
+            <strong>Weight:</strong> {profile.personalDetails?.weight}
           </p>
           <p>
-            <strong>Blood Group:</strong> {profile.personalDetails.blood_group}
+            <strong>Blood Group:</strong> {profile.personalDetails?.blood_group}
           </p>
           <p>
             <strong>Physically Challenged:</strong>{" "}
-            {profile.personalDetails.physically_challenged ? "Yes" : "No"}
+            {profile.personalDetails?.physically_challenged ? "Yes" : "No"}
           </p>
           <p>
             <strong>Physical Challenge Details:</strong>{" "}
-            {profile.personalDetails.physical_challenge_details}
+            {profile.personalDetails?.physical_challenge_details}
           </p>
         </div>
       </Accordion>
