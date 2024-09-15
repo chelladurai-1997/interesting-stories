03aa2989fced0459e406abd1bc92075c90ccc184
chelladurai-1997 <mechelladurai97@gmail.com>
2024-09-15 23:26:09 +0530

    feat: Style update

--- app/profiles/[userId]/page.tsx
+++ app/profiles/[userId]/page.tsx
@@ -199,7 +199,7 @@ const Page: React.FC = () => {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-4 p-6">
       {/* Sticky Image Gallery */}
-      <div className="hidden lg:block lg:w-1/4 lg:sticky lg:top-6 lg:space-y-4">
+      <div className="lg:w-1/4 lg:sticky lg:top-6 lg:space-y-4 mb-4 lg:mb-0">
         <ImageGallery
           images={[
             "https://media.istockphoto.com/id/1283895185/photo/hindu-indian-young-bride-holding-deepa.jpg?s=1024x1024&w=is&k=20&c=vGodoL_hhKP1Q11f7tgFSxddz7O6csnn2AGyitTQ6Yk=",
