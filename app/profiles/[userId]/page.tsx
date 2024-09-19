173ede01ba5c848e2973c2b1ac6756dda1ad48ff
chelladurai-1997 <mechelladurai97@gmail.com>
2024-09-19 11:14:51 +0530

    feat: ui fix

--- app/profiles/[userId]/page.tsx
+++ app/profiles/[userId]/page.tsx
@@ -12,6 +12,7 @@ import ProfileFamilyDetails from "@/app/components/organism/ProfileDetails/Profi
 import ProfileHoroscopeInfo from "@/app/components/organism/ProfileDetails/ProfileHoroscopeInfo";
 import ProfilePersonalDetails from "@/app/components/organism/ProfileDetails/ProfilePersonalDetails";
 import useProfile from "@/app/lib/hooks/services/useProfile";
+import Container from "@/app/components/molecules/Container/Container";
 
 const Page: React.FC = () => {
   const params = useParams();
@@ -25,54 +26,59 @@ const Page: React.FC = () => {
   if (!profile) return <div>No profile data found.</div>;
 
   return (
-    <div className="flex flex-col lg:flex-row lg:space-x-4 p-6">
-      <div className="lg:w-1/4 lg:sticky lg:top-6 lg:space-y-4 mb-4 lg:mb-0">
-        <ImageGallery
-          images={[profile?.contactInfo?.photo, profile?.horoscopeInfo?.upload]}
-        />
-      </div>
-      <div className="flex-1 overflow-y-auto p-4 bg-white shadow-lg rounded-lg">
-        <h1 className="text-2xl font-extrabold text-gray-800 mb-6 text-center">
-          Profile Details
-        </h1>
+    <Container>
+      <div className="flex flex-col lg:flex-row lg:space-x-4 p-6">
+        <div className="lg:w-1/4 lg:sticky lg:top-6 lg:space-y-4 mb-4 lg:mb-0">
+          <ImageGallery
+            images={[
+              profile?.contactInfo?.photo,
+              profile?.horoscopeInfo?.upload,
+            ]}
+          />
+        </div>
+        <div className="flex-1 overflow-y-auto p-4 bg-white shadow-lg rounded-lg">
+          <h1 className="text-2xl font-extrabold text-gray-800 mb-6 text-center">
+            Profile Details
+          </h1>
 
-        <ProfileBasicInfo
-          profile={profile}
-          openSection={openSection}
-          setOpenSection={setOpenSection}
-        />
-        <ProfileContactInfo
-          profile={profile}
-          openSection={openSection}
-          setOpenSection={setOpenSection}
-        />
-        <ProfileEducationOccupation
-          profile={profile}
-          openSection={openSection}
-          setOpenSection={setOpenSection}
-        />
-        <ProfileExpectations
-          profile={profile}
-          openSection={openSection}
-          setOpenSection={setOpenSection}
-        />
-        <ProfileFamilyDetails
-          profile={profile}
-          openSection={openSection}
-          setOpenSection={setOpenSection}
-        />
-        <ProfileHoroscopeInfo
-          profile={profile}
-          openSection={openSection}
-          setOpenSection={setOpenSection}
-        />
-        <ProfilePersonalDetails
-          profile={profile}
-          openSection={openSection}
-          setOpenSection={setOpenSection}
-        />
+          <ProfileBasicInfo
+            profile={profile}
+            openSection={openSection}
+            setOpenSection={setOpenSection}
+          />
+          <ProfileContactInfo
+            profile={profile}
+            openSection={openSection}
+            setOpenSection={setOpenSection}
+          />
+          <ProfileEducationOccupation
+            profile={profile}
+            openSection={openSection}
+            setOpenSection={setOpenSection}
+          />
+          <ProfileExpectations
+            profile={profile}
+            openSection={openSection}
+            setOpenSection={setOpenSection}
+          />
+          <ProfileFamilyDetails
+            profile={profile}
+            openSection={openSection}
+            setOpenSection={setOpenSection}
+          />
+          <ProfileHoroscopeInfo
+            profile={profile}
+            openSection={openSection}
+            setOpenSection={setOpenSection}
+          />
+          <ProfilePersonalDetails
+            profile={profile}
+            openSection={openSection}
+            setOpenSection={setOpenSection}
+          />
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
