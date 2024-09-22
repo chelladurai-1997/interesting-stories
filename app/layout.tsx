d4342ca92eed592b9afbe4e5bbe56f8a16856006
chelladurai-1997 <mechelladurai97@gmail.com>
2024-09-22 23:22:22 +0530

    feat: adding toploader when the route change happens

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "./lib/contexts/UserContext";
-import Footer from "./components/organism/Footer/Footer";
 import { Toaster } from "react-hot-toast";
 import SessionMonitor from "./components/organism/SessionMonitor/SessionMonitor";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
+        <NextTopLoader color="#007BFF" crawl />
         <UserProvider>
           {children}
           <SessionMonitor />
