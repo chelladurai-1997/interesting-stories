76d1b6bcb94835a786badaac778ee172912840f7
chelladurai-1997 <mechelladurai97@gmail.com>
2024-09-29 15:31:46 +0530

    feat: input updation for searching

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Header showSearchForm />
-      {/* <ProfileList /> */}
+      <ProfileList />
       <div className="bg-teal-500 text-gray-900 py-16 px-8">
         <div className="max-w-4xl mx-auto text-center">
           <h1 className="text-4xl font-bold mb-4">
